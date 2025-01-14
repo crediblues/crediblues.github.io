ecec760a157207f2076a676de96cdcedd6a91665
crediblues <crediblues@gmail.com>
2025-01-14 16:51:43 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig = {
   output: 'export',
   basePath: '/crediblues.github.io',
   // assetPrefix: '/crediblues.github.io',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
